420a64972bd0531a6c4e8455feb8967baf8657dd
BORA <a52447879@gmail.com>
2021-11-28 11:46:06 +0900

    스타일입히기

--- main.js
+++ main.js
@@ -6,3 +6,7 @@ const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth - 100;
 canvas.height = window.innerHeight - 100;
+
+// 스타일 입히기
+ctx.fillStyle = 'green';
+ctx.fillRect(10, 10, 100, 100); // 10.10 좌표에 100*100사이즈에 네모그리기
